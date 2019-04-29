c86111634adedb3fda069a543b889b52c078e042
KhaledMohamedNasser <48595371+KhaledMohamedNasser@users.noreply.github.com>
2019-04-29 03:46:38 +0200

    Add files via upload

---
+++ River Crosser/src/commands/NullCommand.java
@@ -0,0 +1,16 @@
+package commands;
+
+public class NullCommand implements Command{
+
+	@Override
+	public void execute() {
+		System.out.println("There is no button here");
+		
+	}
+
+	@Override
+	public void undo() {
+		System.out.println("There is no button here");
+	}
+
+}
