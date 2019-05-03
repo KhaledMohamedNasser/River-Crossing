213a944e7315e5b84ba41f0244c647fdda1c037e
SNwalid <48567301+SNwalid@users.noreply.github.com>
2019-05-03 16:55:44 +0200

    All no img

--- River Crosser/src/GUI/Story2.java
+++ River Crosser/src/GUI/Story2.java
@@ -12,6 +12,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 
 public class Story2 extends JFrame {
 
@@ -38,26 +40,26 @@ public class Story2 extends JFrame {
 	 */
 	public Story2() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 948, 600);
+		setBounds(100, 100, 1114, 688);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		Canvas canvas=new Canvas();
-		canvas.setBounds(0, 0, 930, 462);
-		
-		contentPane.add(canvas);
 		
 		JButton btnNewButton = new JButton("Framer-90");
 		btnNewButton.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 18));
 		btnNewButton.setForeground(Color.BLUE);
-		btnNewButton.setBounds(808, 468, 110, 34);
+		btnNewButton.setBounds(974, 553, 110, 34);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Farmer-40");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
 		btnNewButton_1.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 18));
 		btnNewButton_1.setForeground(Color.BLUE);
-		btnNewButton_1.setBounds(808, 515, 110, 34);
+		btnNewButton_1.setBounds(974, 599, 110, 34);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Farmer-80");
@@ -67,13 +69,13 @@ public class Story2 extends JFrame {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		btnNewButton_2.setBounds(661, 466, 110, 34);
+		btnNewButton_2.setBounds(852, 553, 110, 34);
 		contentPane.add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("Farmer-60");
 		btnNewButton_3.setForeground(Color.BLUE);
 		btnNewButton_3.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 18));
-		btnNewButton_3.setBounds(661, 515, 110, 34);
+		btnNewButton_3.setBounds(852, 599, 110, 34);
 		contentPane.add(btnNewButton_3);
 		
 		JButton btnNewButton_4 = new JButton("Exit");
@@ -84,32 +86,101 @@ public class Story2 extends JFrame {
 		});
 		btnNewButton_4.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 18));
 		btnNewButton_4.setForeground(Color.BLUE);
-		btnNewButton_4.setBounds(12, 515, 97, 31);
+		btnNewButton_4.setBounds(10, 601, 97, 31);
 		contentPane.add(btnNewButton_4);
 		
 		JButton btnNewButton_5 = new JButton("Reset");
 		btnNewButton_5.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 18));
 		btnNewButton_5.setForeground(Color.BLUE);
-		btnNewButton_5.setBounds(121, 515, 97, 31);
+		btnNewButton_5.setBounds(119, 601, 97, 31);
 		contentPane.add(btnNewButton_5);
 		
 		JButton btnNewButton_6 = new JButton("Wolf");
 		btnNewButton_6.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 18));
 		btnNewButton_6.setForeground(Color.BLUE);
-		btnNewButton_6.setBounds(523, 489, 97, 34);
+		btnNewButton_6.setBounds(743, 575, 97, 34);
 		contentPane.add(btnNewButton_6);
 		
 		JButton btnNewButton_7 = new JButton("Move");
 		btnNewButton_7.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 18));
 		btnNewButton_7.setForeground(Color.BLUE);
-		btnNewButton_7.setBounds(412, 489, 97, 34);
+		btnNewButton_7.setBounds(497, 575, 97, 34);
 		contentPane.add(btnNewButton_7);
 		
 		JButton btnNewButton_8 = new JButton("Save");
 		btnNewButton_8.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 18));
 		btnNewButton_8.setForeground(Color.BLUE);
-		btnNewButton_8.setBounds(10, 468, 97, 31);
+		btnNewButton_8.setBounds(10, 555, 97, 31);
 		contentPane.add(btnNewButton_8);
-		//setContentPane(contentPane);
-	}
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setIcon(new ImageIcon(Story2.class.getResource("/farmerBigRight.png")));
+		lblNewLabel_1.setBounds(1017, 439, 67, 88);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(new ImageIcon(Story2.class.getResource("/farmerRight1.png")));
+		lblNewLabel_2.setBounds(956, 432, 65, 78);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setIcon(new ImageIcon(Story2.class.getResource("/farmerR3.png")));
+		lblNewLabel_3.setBounds(918, 419, 56, 71);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("New label");
+		lblNewLabel_4.setIcon(new ImageIcon(Story2.class.getResource("/farmer5.png")));
+		lblNewLabel_4.setBounds(852, 399, 67, 88);
+		contentPane.add(lblNewLabel_4);
+		  
+		JLabel lblNewLabel_5 = new JLabel("");
+		lblNewLabel_5.setIcon(new ImageIcon(Story2.class.getResource("/Black_Right.png")));
+		lblNewLabel_5.setBounds(791, 399, 56, 65);
+		contentPane.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("");
+		lblNewLabel_6.setIcon(new ImageIcon(Story2.class.getResource("/farmerBigLeft.png")));
+		lblNewLabel_6.setBounds(10, 422, 67, 88);
+		contentPane.add(lblNewLabel_6);
+		lblNewLabel_6.hide();
+		
+		JLabel lblNewLabel_7 = new JLabel("");
+		lblNewLabel_7.setIcon(new ImageIcon(Story2.class.getResource("/farmerLeft1.png")));
+		lblNewLabel_7.setBounds(89, 419, 65, 78);
+		contentPane.add(lblNewLabel_7);
+		lblNewLabel_7.hide();
+		
+		JLabel lblNewLabel_8 = new JLabel("");
+		lblNewLabel_8.setIcon(new ImageIcon(Story2.class.getResource("/farmerL3.png")));
+		lblNewLabel_8.setBounds(140, 399, 76, 88);
+		contentPane.add(lblNewLabel_8);
+		lblNewLabel_8.hide();
+		
+		JLabel lblNewLabel_9 = new JLabel("");
+		lblNewLabel_9.setIcon(new ImageIcon(Story2.class.getResource("/farmer5.png")));
+		lblNewLabel_9.setBounds(194, 383, 76, 88);
+		contentPane.add(lblNewLabel_9);
+		lblNewLabel_9.hide();
+		 
+		JLabel lblNewLabel_10 = new JLabel("");
+		lblNewLabel_10.setIcon(new ImageIcon(Story2.class.getResource("/Black_Left.png")));
+		lblNewLabel_10.setBounds(263, 399, 56, 65);
+		contentPane.add(lblNewLabel_10);
+		lblNewLabel_10.hide();
+		
+		JLabel lblNewLabel_11 = new JLabel("");
+		lblNewLabel_11.setIcon(new ImageIcon(Story2.class.getResource("/Raft.png")));
+		lblNewLabel_11.setBounds(707, 483, 170, 68);
+		contentPane.add(lblNewLabel_11);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(Story2.class.getResource("/RiverBig.png")));
+		lblNewLabel.setBounds(0, 0, 1096, 540);
+		contentPane.add(lblNewLabel);
+		
+
+		
+		
+
+    }
 }
--- River Crosser/src/GUI/playFrame.java
+++ River Crosser/src/GUI/playFrame.java
@@ -6,32 +6,25 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.text.html.ImageView;
-
 import org.ietf.jgss.GSSContext;
-
 import controller.Controller;
-
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GraphicsConfiguration;
-import java.awt.Image;
-import java.awt.Window;
-import java.awt.event.ActionListener;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
-import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 public class playFrame extends JFrame {
 
 	private JPanel contentPane;
+	boolean dos=true;
 	private Controller controller=new Controller();
 	/**
 	 * Launch the application.
@@ -78,9 +71,48 @@ public class playFrame extends JFrame {
 		
 		JLabel lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2.setIcon(new ImageIcon(playFrame.class.getResource("/WolfRight.png")));
-		lblNewLabel_2.setBounds(848, 384, 56, 62);
+		lblNewLabel_2.setBounds(869, 384, 56, 62);
 		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_4 = new JLabel("");
+		lblNewLabel_4.setIcon(new ImageIcon(playFrame.class.getResource("/goatRight.png")));
+		lblNewLabel_4.setBounds(798, 360, 75, 74);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_6 = new JLabel("");
+		lblNewLabel_6.setIcon(new ImageIcon(playFrame.class.getResource("/carrots.png")));
+		lblNewLabel_6.setBounds(720, 372, 56, 37);
+		contentPane.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("");
+		lblNewLabel_7.setBounds(717, 340, 75, 74);
+		contentPane.add(lblNewLabel_7);
+		JLabel lblNewLabel_5 = new JLabel("New label");
+		lblNewLabel_5.setIcon(new ImageIcon(playFrame.class.getResource("/farmerLeft.png")));
+		lblNewLabel_5.setBounds(66, 356, 97, 90);
+		contentPane.add(lblNewLabel_5);
+		JLabel lblNewLabel_8 = new JLabel("");
+		lblNewLabel_8.setIcon(new ImageIcon(playFrame.class.getResource("/wolfLeft.png")));
+		lblNewLabel_8.setBounds(140, 372, 56, 62);
+		contentPane.add(lblNewLabel_8);
+		JLabel lblNewLabel_9 = new JLabel("");
+		lblNewLabel_9.setIcon(new ImageIcon(playFrame.class.getResource("/goatLeft.png")));
+		lblNewLabel_9.setBounds(208, 360, 75, 74);
+		contentPane.add(lblNewLabel_9);
+		
+		JLabel lblNewLabel_10 = new JLabel("");
+		lblNewLabel_10.setIcon(new ImageIcon(playFrame.class.getResource("/carrots.png")));
+		lblNewLabel_10.setBounds(268, 372, 56, 37);
+		contentPane.add(lblNewLabel_10);
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(playFrame.class.getResource("/Raft.png")));
+		lblNewLabel.setBounds(660, 426, 170, 68);
+		contentPane.add(lblNewLabel);
 		JButton btnNewButton = new JButton("Exit");
+		lblNewLabel_5.hide();
+		lblNewLabel_8.hide();
+		lblNewLabel_9.hide();
+		lblNewLabel_10.hide();
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
@@ -105,6 +137,144 @@ public class playFrame extends JFrame {
 		JButton btnNewButton_6 = new JButton("Move");
 		btnNewButton_6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+																							//here u should check if the boat able to move or not  
+				if(dos) {
+				Timer time=new Timer();
+				TimerTask timer=new TimerTask() {
+					
+					@Override
+					public void run() {
+				    	lblNewLabel.setBounds(550, 426, 170, 68);  							//moving the boat 		
+						lblNewLabel_1.setBounds(550, 380, 97, 90); 							// the thing what u want it to move
+
+					}
+				};
+				time.schedule(timer,100);
+				 timer=new TimerTask() {
+					
+					@Override
+					public void run() {
+						lblNewLabel.setBounds(500, 426, 170, 68);
+						lblNewLabel_1.setBounds(500, 380, 97, 90);
+					}
+
+				};
+				time.schedule(timer,200);
+				timer =new TimerTask() {
+					
+					@Override
+					public void run() {
+						lblNewLabel.setBounds(440, 426, 170, 68);
+						lblNewLabel_1.setBounds(440, 380, 97, 90);
+					}
+				};
+				time.schedule(timer, 300);
+				timer =new TimerTask() {
+					
+					@Override
+					public void run() {
+						lblNewLabel.setBounds(400, 426, 170, 68);
+						lblNewLabel_1.setBounds(400, 380, 97, 90);
+					}
+				};
+				time.schedule(timer, 400);
+				timer =new TimerTask() {
+					
+					@Override
+					public void run() {
+						lblNewLabel.setBounds(300, 426, 170, 68);
+						lblNewLabel_1.setBounds(300, 380, 97, 90);
+					}
+				};
+				time.schedule(timer, 500);
+				timer =new TimerTask() {
+					
+					@Override
+					public void run() {
+						lblNewLabel.setBounds(200, 426, 170, 68);
+						lblNewLabel_1.setBounds(200, 380, 97, 90);
+					}
+				};
+				time.schedule(timer, 600);
+				timer =new TimerTask() {					
+					@Override
+					public void run() {
+						lblNewLabel_5.show();									 //show the left object	
+						lblNewLabel_1.hide(); 									//hide the right object 
+						dos=false;
+					}
+				};
+				time.schedule(timer, 700);
+			}
+			else {
+				Timer time=new Timer();
+				TimerTask timer=new TimerTask() {
+					
+					@Override
+					public void run() {
+						lblNewLabel.setBounds(200, 426, 170, 68);
+						//lblNewLabel_1.setBounds(200, 380, 97, 90);
+
+					}
+				};
+				time.schedule(timer,100);
+				 timer=new TimerTask() {
+					
+					@Override
+					public void run() {
+						lblNewLabel.setBounds(300, 426, 170, 68);
+						//lblNewLabel_1.setBounds(300, 380, 97, 90);
+					}
+
+				};
+				time.schedule(timer,200);
+				timer =new TimerTask() {
+					
+					@Override
+					public void run() {
+						lblNewLabel.setBounds(400, 426, 170, 68);
+						//lblNewLabel_1.setBounds(400, 380, 97, 90);
+					}
+				};
+				time.schedule(timer, 300);
+				timer =new TimerTask() {
+					
+					@Override
+					public void run() {
+						lblNewLabel.setBounds(440, 426, 170, 68);
+						//lblNewLabel_1.setBounds(440, 380, 97, 90);
+					}
+				};
+				time.schedule(timer, 400);
+				timer =new TimerTask() {
+					
+					@Override
+					public void run() {
+						lblNewLabel.setBounds(500, 426, 170, 68);
+						//lblNewLabel_1.setBounds(500, 380, 97, 90);
+					}
+				};
+				time.schedule(timer, 500);
+				timer =new TimerTask() {
+					
+					@Override
+					public void run() {
+						lblNewLabel.setBounds(660, 426, 170, 68);
+						//lblNewLabel_1.setBounds(660, 380, 97, 90);
+					}
+				};
+				time.schedule(timer, 600);
+				timer =new TimerTask() {
+					
+					@Override
+					public void run() {
+						lblNewLabel_5.show();
+						lblNewLabel_1.hide();
+						dos=true;
+					}
+				};
+				time.schedule(timer, 700);
+			}
 			}
 		});
 		btnNewButton_6.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 18));
@@ -115,7 +285,13 @@ public class playFrame extends JFrame {
 		JButton btnNewButton_2 = new JButton("Farmer");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				lblNewLabel_1.setBounds(670, 380, 97, 90);
+				if(lblNewLabel_1.getBounds().contains(916, 384))
+				  lblNewLabel_1.setBounds(670, 380, 97, 90);
+				else if(lblNewLabel_1.getBounds().contains(200, 380))
+					lblNewLabel_5.setBounds(200, 380, 97, 90);
+				else
+					lblNewLabel_1.setBounds(916, 384, 97, 90);
+
 				
 			}
 		});
@@ -127,30 +303,25 @@ public class playFrame extends JFrame {
 		JButton btnNewButton_3 = new JButton("Wolf");
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				lblNewLabel_2.setBounds(747, 404, 56, 62);
+				
 			}
 		});
 		btnNewButton_3.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 18));
 		btnNewButton_3.setForeground(Color.BLUE);
-		btnNewButton_3.setBounds(788, 545, 116, 37);
+		btnNewButton_3.setBounds(747, 545, 116, 37);
 		contentPane.add(btnNewButton_3);
 		
-		JButton btnNewButton_4 = new JButton("Black Wolf");
-		btnNewButton_4.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 18));
-		btnNewButton_4.setForeground(Color.BLUE);
-		btnNewButton_4.setBounds(660, 545, 116, 37);
-		contentPane.add(btnNewButton_4);
-		
 		JButton btnNewButton_5 = new JButton("Goat");
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				lblNewLabel_4.setBounds(755, 400, 75, 74);;
+			}
+		});
 		btnNewButton_5.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 18));
 		btnNewButton_5.setForeground(Color.BLUE);
-		btnNewButton_5.setBounds(532, 545, 116, 37);
+		btnNewButton_5.setBounds(593, 545, 116, 37);
 		contentPane.add(btnNewButton_5);
-		
-		JButton btnNewButton_7 = new JButton("Sheep");
-		btnNewButton_7.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 18));
-		btnNewButton_7.setForeground(Color.BLUE);
-		btnNewButton_7.setBounds(404, 546, 116, 37);
-		contentPane.add(btnNewButton_7);
 		
 		JButton btnNewButton_8 = new JButton("Save");
 		btnNewButton_8.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 18));
@@ -158,31 +329,25 @@ public class playFrame extends JFrame {
 		btnNewButton_8.setBounds(10, 498, 97, 37);
 		contentPane.add(btnNewButton_8);
 		
-		JLabel lblNewLabel_4 = new JLabel("");
-		lblNewLabel_4.setIcon(new ImageIcon(playFrame.class.getResource("/goatRight.png")));
-		lblNewLabel_4.setBounds(788, 372, 75, 74);
-		contentPane.add(lblNewLabel_4);
-		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(playFrame.class.getResource("/Raft.png")));
-		lblNewLabel.setBounds(660, 426, 170, 68);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblNewLabel_6 = new JLabel("");
-		lblNewLabel_6.setIcon(new ImageIcon(playFrame.class.getResource("/carrots.png")));
-		lblNewLabel_6.setBounds(720, 372, 56, 37);
-		contentPane.add(lblNewLabel_6);
-		
-		JLabel lblNewLabel_7 = new JLabel("");
-		lblNewLabel_7.setBounds(717, 340, 75, 74);
-		contentPane.add(lblNewLabel_7);
+
 		
 		JLabel lblNewLabel_3 = new JLabel("");
 		lblNewLabel_3.setIcon(new ImageIcon(playFrame.class.getResource("/River_1.png")));
 		lblNewLabel_3.setBounds(0, 0, 1044, 484);
 		contentPane.add(lblNewLabel_3);
 		
+		JButton btnNewButton_4 = new JButton("Plant");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				lblNewLabel_6.setBounds(747,426, 56, 37);
+			}
+		});
+		btnNewButton_4.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 18));
+		btnNewButton_4.setForeground(Color.BLUE);
+		btnNewButton_4.setBounds(443, 545, 97, 37);
+		contentPane.add(btnNewButton_4);
 		
 
+		
 	}
 }
